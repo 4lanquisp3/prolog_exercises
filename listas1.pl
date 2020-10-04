87f1b3368dff977afa0c0b664086053e11b5c362
aland3v <aland3v@gmail.com>
2020-10-04 12:29:14 -0400

    Se adiciono el programa que verifica si el ultimo elemento de una lista es un X dado

--- listas1.pl
+++ listas1.pl
@@ -1,3 +1,8 @@
 /* verificar si un elemento pertenece a una lista */
 pertenece(X,[X|_]).
 pertenece(X,[_|Y]):-pertenece(X,Y).
+
+/* X es el ultimo elemento de Y */
+ultimo(X,[X]).
+ultimo(X,[_|Y]):-ultimo(X,Y).
+
